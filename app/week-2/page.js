aefb7ff9c9b0f3a90776c8ca235824baac3db487
Vincent Manimtim <leighvincent.manimtim@edu.sait.ca>
2025-09-09 14:01:57 -0600

    Started Week 2 Assignment
    I cleaned up the home page and created a  new page and component

---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+
+
+
+export default function MySecondPage(){
+     
+
+    return(
+
+    <main>
+        <h1>This is the Second Page</h1>
+    </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+
+export default function MyInfo(){
+
+
+    return(
+        <main>
+            <h1>Name: Vincent Manimtim </h1>
+            <ul>
+                <li><Link href="">My Github</Link></li>
+            </ul>
+        </main>
+    );
+}
