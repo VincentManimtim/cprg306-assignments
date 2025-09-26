b08f3a78c2a73aea85cf934d896878a80affa428
Vincent <leighvincent.manimtim@edu.sait.ca>
2025-09-26 12:16:21 -0600

    week-4-assignment
    Made a page with a increment and decrement function

---
+++ app/week-4/new-item.js
@@ -0,0 +1,44 @@
+" use client"
+
+import { useState } from "react"
+
+export default function NewItem(){
+    
+    const [count, setCount] = useState(0); 
+
+    const plus = () => {
+        if (count < 20){
+            setCount(count +1);
+        }
+    }
+
+    const minus = () => {
+        if (count > 1){
+            setCount(count - 1);
+        }
+    };
+
+    let buttonGreen = "bg-green-700 hover:bg-green-500 active:bg-green-700 text-white rounded size-15 ml-10 mr-2 cursor-pointer transition-colors "
+
+    let buttonRed = "bg-red-700 hover:bg-red-300 active:bg-red-700 text-white rounded size-15 cursor-pointer transition-colors "
+
+       if (count >= 20){
+        buttonGreen = "bg-gray-400 text-white rounded size-15 ml-10 mr-2"
+    }
+
+    if (count == 1){
+        buttonRed = "bg-gray-400 text-white rounded size-15"
+    }
+
+    return (
+        <main className="bg-blue-300 p-3 rounded-4xl m-5 size-fit">
+            <p className="text-4xl"> {count}
+                <button onClick={plus} className= {buttonGreen}> + </button>
+                <button onClick={minus} className= {buttonRed}> - </button>
+            </p>
+        
+        </main>
+    )
+
+}
+
---
+++ app/week-4/page.js
@@ -0,0 +1,12 @@
+"use client"
+import NewItem from "./new-item";
+
+
+
+export default function WeekFour(){
+
+
+    return(
+        <main><NewItem/></main>
+    )
+}
